b499abb44bf022ce879e9d0eef1a38522f090e82
colee222 <nfsulli@gmail.com>
2023-04-21 16:31:07 -0700

    first commit from new install

---
+++ GeekforGeeks-ProblemoftheDay/setup-ubuntu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#mkdir Projects
+#cd Projects 
+#mkdir GeekforGeeks-ProblemoftheDay
+#sudo apt-get update
+#sudo apt-get install git
+#git init
+#git remote add origin https://github.com/colee222/GeekforGeeks-ProblemoftheDay.git
+
+#sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo apt-get update
+#sudo apt-get install python3.8
+#alias python=python3.8
+# alias python3=python3.8
+# sudo apt install python3-pip
+# pip3 install --upgrade pip setuptools
+
+#sudo snap install --classic code
+
+# sudo snap install jupyter
+
+git config --global user.email "nfsulli@gmail.com"
+git config --global user.name "colee222"
+git add . 
+git commit -m "first commit from new install"
+git push -u add origin main
+ 
